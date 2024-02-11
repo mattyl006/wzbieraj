5ec6090c7ff9d0ef3c787848ff88b9a8ba46d852
Mateusz Tylka <mattyl@st.amu.edu.pl>
2024-02-11 10:36:22 +0100

    mobile sidemenu init

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 console.log("siema")
 
-const navMenu = document.querySelector('.navbar__nav-button-mobile');
+const navButton = document.querySelector('.navbar__button-mobile');
 const hamburger = document.querySelector('.navbar__hamburger-mobile');
 const hamburgerCross = document.querySelector('.navbar__hamburger-active-mobile');
 
-navMenu.addEventListener("click", () => {
+const navBarMenu = document.querySelector('.navbar__menu-mobile--js');
+
+navButton.addEventListener("click", () => {
     hamburger.classList.toggle('show');
     hamburgerCross.classList.toggle('show');
+    navBarMenu.classList.toggle('navbar__menu-mobile--show');
 });
